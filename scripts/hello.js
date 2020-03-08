74b55270900474d164439fab7ac871091031cd80
kujira_desu <test@test.hoge.hoge>
2020-03-08 15:56:24 +0900

    おみくじを実装。（TODO:毎回同じ結果が出る）

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,43 @@
+/**
+ * Slackチャンネルに[おみくじ]と入れたら、ランダムの結果を返すBot
+ * 
+ * [yarn install]はエラーになるので、[yarn install --no-bin-links]を実行
+ */
+
+/**
+ * インストール
+ * yarn global add yo@3.0.0
+ * yarn global add generator-hubot
+ * yarn global add coffeescript@1.12.7
+ * 
+ * cd ~/workspace
+ * mkdir hubot-study
+ * cd hubot-study
+ * 
+ * yo hubot
+ * を実行。
+ * だいたいデフォルト。[Bot adapter]のみ[slack]を入力
+ * エラー出て[Dane]にならなかった
+ * yarn global add generator-hubot-yarn
+ * yo hubot-yarn
+ * で[Overwrite ～] に yで返答したら[Dane]になった
+ * 
+ * 単体テストは[bin/hubot]で起動
+ * [hello>]を入れて[Hello, <@1>]が返ってくればOK
+ * 
+ * サーバでの起動は
+ * slackと連携するには、slackのワークスペースにHubotを追加して[HUBOT_SLACK_TOKEN=xoxb-xxxx～]と書かれたトークンを取得
+ * チャンネルにHubotを追加する
+ * env HUBOT_SLACK_TOKEN=xoxb-jdlasjdfaijfefjljlfkdsjfliejlfejlfaelij bin/hubot --adapter slack
+ * を実行
+ */
 'use strict';
+const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+const lot = lots[Math.floor(Math.random() * lots.length)];
+
 module.exports = (robot) => {
-	robot.hear(/hello>/i, (msg) => {
+	robot.hear(/おみくじ/i, (msg) => {
 		const username = msg.message.user.name;
-		msg.send('Hello, ' + username);
+		msg.send(username + 'の運勢は' + lot + 'です。');
 	});
 };
